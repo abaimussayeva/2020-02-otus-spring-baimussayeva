7779df2d2edeb2e0858e5d9f6a9f8a5a36b38ea1
Aigul Baimussayeva <a.baymusaeva@arta.pro>
2020-04-06 14:58:12 +0600

    task 07 jdbc remove unused constants

--- task-02/src/main/java/ru/otus/spring/hw/domain/model/Messages.java
+++ task-02/src/main/java/ru/otus/spring/hw/domain/model/Messages.java
@@ -1,10 +1,6 @@
 package ru.otus.spring.hw.domain.model;
 
 public class Messages {
-    public static final String GENRE = "Жанр";
     public static final String Y = "1";
     public static final String N = "0";
-    public static final String AUTHOR = "Автор";
-    public static final String BOOK_NAME = "Название книги";
-    public static final String LANG = "Язык";
 }

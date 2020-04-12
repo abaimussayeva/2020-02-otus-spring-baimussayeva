d30b9a3cd4f1219a1225ef576d68546d6c63dcbc
Aigul Baimussayeva <a.baymusaeva@arta.pro>
2020-04-12 15:28:14 +0600

    task 11

--- task-02/src/main/java/ru/otus/spring/hw/application/business/services/CommentServiceImpl.java
+++ task-02/src/main/java/ru/otus/spring/hw/application/business/services/CommentServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.otus.spring.hw.application.business.services;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.otus.spring.hw.application.business.repository.CommentRepository;
 import ru.otus.spring.hw.domain.business.services.CommentService;
 import ru.otus.spring.hw.domain.errors.DBOperationException;
@@ -9,7 +8,6 @@ import ru.otus.spring.hw.domain.model.Book;
 import ru.otus.spring.hw.domain.model.Comment;
 
 @Service
-@Transactional(readOnly = true)
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
